11f9a3ea8157c4e9fbfe604fef777cc1cb9ef763
shockyng <shockyng@icloud.com>
2025-10-02 19:36:40 +0100

    sanitize resolution

--- src/main/java/org/magnum/dataup/VideosController.java
+++ src/main/java/org/magnum/dataup/VideosController.java
@@ -1,19 +1,19 @@
 /*
- * 
+ *
  * Copyright 2014 Jules White
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  */
 package org.magnum.dataup;
 
@@ -45,24 +45,26 @@ import static org.magnum.dataup.VideoSvcApi.VIDEO_DATA_PATH;
 @Controller
 public class VideosController {
 
-	/**
-	 * You will need to create one or more Spring controllers to fulfill the
-	 * requirements of the assignment. If you use this file, please rename it
-	 * to something other than "AnEmptyController"
-	 * 
-	 * 
-		 ________  ________  ________  ________          ___       ___  ___  ________  ___  __       
-		|\   ____\|\   __  \|\   __  \|\   ___ \        |\  \     |\  \|\  \|\   ____\|\  \|\  \     
-		\ \  \___|\ \  \|\  \ \  \|\  \ \  \_|\ \       \ \  \    \ \  \\\  \ \  \___|\ \  \/  /|_   
-		 \ \  \  __\ \  \\\  \ \  \\\  \ \  \ \\ \       \ \  \    \ \  \\\  \ \  \    \ \   ___  \  
-		  \ \  \|\  \ \  \\\  \ \  \\\  \ \  \_\\ \       \ \  \____\ \  \\\  \ \  \____\ \  \\ \  \ 
-		   \ \_______\ \_______\ \_______\ \_______\       \ \_______\ \_______\ \_______\ \__\\ \__\
-		    \|_______|\|_______|\|_______|\|_______|        \|_______|\|_______|\|_______|\|__| \|__|
-                                                                                                                                                                                                                                                                        
-	 * 
-	 */
+    /**
+     * You will need to create one or more Spring controllers to fulfill the
+     * requirements of the assignment. If you use this file, please rename it
+     * to something other than "AnEmptyController"
+     * <p>
+     * <p>
+     * ________  ________  ________  ________          ___       ___  ___  ________  ___  __
+     * |\   ____\|\   __  \|\   __  \|\   ___ \        |\  \     |\  \|\  \|\   ____\|\  \|\  \
+     * \ \  \___|\ \  \|\  \ \  \|\  \ \  \_|\ \       \ \  \    \ \  \\\  \ \  \___|\ \  \/  /|_
+     * \ \  \  __\ \  \\\  \ \  \\\  \ \  \ \\ \       \ \  \    \ \  \\\  \ \  \    \ \   ___  \
+     * \ \  \|\  \ \  \\\  \ \  \\\  \ \  \_\\ \       \ \  \____\ \  \\\  \ \  \____\ \  \\ \  \
+     * \ \_______\ \_______\ \_______\ \_______\       \ \_______\ \_______\ \_______\ \__\\ \__\
+     * \|_______|\|_______|\|_______|\|_______|        \|_______|\|_______|\|_______|\|__| \|__|
+     *
+     *
+     */
 
     private final VideoFileManager videoDataMgr = VideoFileManager.get();
+    private static final AtomicLong currentId = new AtomicLong(0L);
+    private final Map<Long, Video> videos = new HashMap<>();
 
     public VideosController() throws IOException {
     }
@@ -88,7 +90,7 @@ public class VideosController {
         Video video = videos.get(id);
         try {
             videoDataMgr.hasVideoData(video);
-        } catch (Exception e ) {
+        } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
 
@@ -104,22 +106,14 @@ public class VideosController {
         ServletOutputStream outputStream = response.getOutputStream();
         try {
             videoDataMgr.copyVideoData(videos.get(id), outputStream);
-        } catch (Exception e ) {
+        } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
 
         return ResponseEntity.ok().build();
     }
 
-    private static final AtomicLong currentId = new AtomicLong(0L);
 
-    private Map<Long, Video> videos = new HashMap<Long, Video>();
-
-    public Video save(Video entity) {
-        checkAndSetId(entity);
-        videos.put(entity.getId(), entity);
-        return entity;
-    }
 
     private void checkAndSetId(Video entity) {
         if (entity.getId() == 0) {
@@ -128,17 +122,16 @@ public class VideosController {
     }
 
     private String getDataUrl(long videoId) {
-        String url = getUrlBaseForLocalServer() + "/video/" + videoId + "/data";
-        return url;
+        return getUrlBaseForLocalServer() + "/video/" + videoId + "/data";
     }
 
     private String getUrlBaseForLocalServer() {
         HttpServletRequest request =
                 ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-        String base =
-                "http://" + request.getServerName()
-                        + ((request.getServerPort() != 80) ? ":" + request.getServerPort() : "");
-        return base;
+
+        return "http://" + request.getServerName()
+                + ((request.getServerPort() != 80) ? ":" + request.getServerPort() : "");
+
     }
-	
+
 }
